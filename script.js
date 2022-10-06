4d5f3fd660f67b693782ef2a641c5c132312929d
Kunal Yadav <34987239+metakunal@users.noreply.github.com>
2022-10-06 22:28:41 +0530

    Revert "add random quote function just styling is left"

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
-let quoteText = document.getElementById('quote')
-let author = document.getElementById('author')
-let btn = document.getElementById('btn')
-
-function randQuote(){
-    btn.classList.add("loading")
-    btn.innerHTML="Loading Quote....";
-    fetch("https://api.quotable.io/random").then(res => res.json()).then(result=> {
-        quoteText.innerText = result.content
-        author.innerHTML = result.author
-        btn.innerHTML = "New Quote"
+// $(document).ready(function () {
+$('button').click(function () {
+    const name = $('#btn').val();
+    var url = "https://codeforces.com/api/user.status?handle=" + name + "&from=1&count=1000000000";
+    fetch(url).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        // console.log(data);
+        const length = data.result.length;
+        var a = 0;
+        for (i = 0; i < length; i++) {
+            if (data.result[i].problem.name === "Anton and Polyhedrons") {
+                a = 1;
+            }
+        }
+        console.log(a);
+        if (a == 1) {
+            document.querySelector('#btn2').innerHTML = 'Solved';
+            // document.querySelector('.btn2').style.background = 'e7e7e7';
+            document.querySelector("#btn2").className = 'solved';
+            // background-color: #4CAF50;
+        }
+        else {
+            document.querySelector('#btn2').innerHTML = 'Solve';
+            document.querySelector('#btn2').className = 'solve';
+        }
+    }).catch(function (error) {
+        document.querySelector('#err').innerHTML = 'Invalid Username';
+        console.log(error);
     })
-}
 
-btn.addEventListener("click", randQuote)
+});
+// });
